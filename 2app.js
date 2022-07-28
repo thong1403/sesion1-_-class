414dd663d2fce8218658c82dad801bd15f7c2400
thong1403 <110025148+thong1403@users.noreply.github.com>
2022-07-28 17:10:27 +0700

    bài tap

---
+++ 2app.js
@@ -0,0 +1,56 @@
+// bài tập 2;
+
+//alert("hello world);
+
+//a: khai báo biến
+// let msg = "rikker is awesome";
+// console.log(msg);
+
+//b :let count = 0;
+// console.log(count);
+
+
+//3
+//a :let msg  = "coding is hard but super cool and fun";
+// console.log(msg);
+
+//b  let count = 30;
+// console.log(count);
+
+// c :let msg = "coding is hard but super cool and fun";
+// let newmsg = msg.toUpperCase();
+// console.log(newmsg);
+
+//d: let number = 0;
+// let number2 = prompt("cho người dùng nhập số n");
+// console.log(number2+number);
+
+ 
+//4: alert("hello Baby");
+
+//5: let name = prompt("cho người dùng nhập tên");
+// console.log("hello " + name);
+
+//6: let first = prompt("cho người dùng nhập tên đầu của họ");
+// let name = prompt("cho người dùng nhập vào tên của họ");
+// console.log("hi " + first  +  name);
+
+//7: let number = prompt("cho người dùng nhập 1 cạnh cuẢ hình vuông");
+// let s = (number * number);
+// console.log(s);
+// alert("diện tích là" +  s);
+
+//8: let a = prompt("nhập bán kính đường tròn");
+// let s = (Math.PI * a * a);
+// alert("diện tích hình tròn là " + s);
+
+let a = prompt("cho người dùng nhập độ c");
+let b = (1.8 * a +32);
+alert("chuyển đổ từ độ c sang độ f " + b);
+
+
+
+
+
+
+
